8dd987b55295850b4035460c1812b287fa50769a
Om Mallick <79105368+ommallick02@users.noreply.github.com>
2024-11-13 21:57:16 +0530

    Updated Model from VGG16 to VGG19

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -14,7 +14,7 @@ class PrepareBaseModel:
 
     
     def get_base_model(self):
-        self.model = tf.keras.applications.vgg16.VGG16(
+        self.model = tf.keras.applications.vgg19.VGG19(
             input_shape=self.config.params_image_size,
             weights=self.config.params_weights,
             include_top=self.config.params_include_top
